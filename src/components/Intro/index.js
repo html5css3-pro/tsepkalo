48746bdd841d6fd52e5a7c101de391d30915ccae
Ilyas Aderiho <Ilya.Adheriho@ibm.com>
2020-05-26 23:27:33 +0300

    Attached links to intro component icons

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -21,21 +21,21 @@ const Intro = () => (
       <section className={style.introAction}>
         <ul>
           <li>
-            <a href="/">
+            <a href="https://yandex.by/maps/157/minsk/?ll=27.800976%2C53.913133&mode=usermaps&source=constructorLink&um=constructor%3A423edbdc5e55d0be632cb70b21d02d16989fadaad55ca7287578e398ccb3ad34&z=10" target="_blank" rel="noopener noreferrer">
               <span><i className="fas fa-hand-peace"></i></span>
               <p>Поставить подпись</p>
             </a>
           </li>
           <li>
-            <a href="/">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfcmR1hjDQsorWHtliOMIaQHFB9Jrie0-t5aHs3LvvHY9uj8g/viewform" target="_blank" rel="noopener noreferrer">
               <span><i className="fas fa-user-plus"></i></span>
               <p>Стать волонтером</p>
             </a>
           </li>
           <li>
-            <a href="/">
-              <span><i className="fas fa-users"></i></span>
-              <p>Инициативная группа</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeskjggOvDNXFadzENmErZbepQ-edeBTSrjgwzU4LHEysHp6A/viewform" target="_blank" rel="noopener noreferrer">
+              <span><i className="fas fa-question-circle"></i></span>
+              <p>Задать вопрос</p>
             </a>
           </li>
         </ul>
