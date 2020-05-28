d3dca6029b3977553a45fa9edc7db54a66769506
Raman <raman.mastyka@gmail.com>
2020-05-28 10:54:55 +0300

    add map iframe

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import style from './Map.module.scss'
+import React from 'react';
+import style from './Map.module.scss';
 
 const Map = () => (
   <section className={style.map}>
-
+    <h2>Карта пикетов:</h2>
+    <div>
+    <iframe
+      title="yamap"
+      src="https://yandex.by/map-widget/v1/-/CCQgUNgAKB"
+      allowFullScreen
+    ></iframe>
+    </div>
   </section>
-)
+);
 
-export default Map
+export default Map;
